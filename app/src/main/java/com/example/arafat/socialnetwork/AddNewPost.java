5eb722f44cfebe14d55dac569f4eb2afa3b1ac74
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-18 19:38:50 +0600

    work with add new post

--- app/src/main/java/com/example/arafat/socialnetwork/AddNewPost.java
+++ app/src/main/java/com/example/arafat/socialnetwork/AddNewPost.java
@@ -2,12 +2,21 @@ package com.example.arafat.socialnetwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class AddNewPost extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_post);
+
+        mToolbar = findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Update Post");
     }
+
 }
